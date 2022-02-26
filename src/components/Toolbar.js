ee5392dad01a62266714e881c04f937659ddc116
Indre-Irb <indre.irbinskaite@gmail.com>
2022-02-26 19:37:43 +0200

    update

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,10 +1,16 @@
 import React, {useContext} from 'react';
 import {Link} from "react-router-dom";
 import MyContext from "../context/MyContext";
+import http from "../plugins/http";
 
 const Toolbar = () => {
 
     const {getUser} = useContext(MyContext)
+
+    async function loggout(){
+        http.get(`loggout`).then(res => {
+        })
+    }
 
     return (
         <div className="toolbar d-flex f-column j-btw">
@@ -17,15 +23,17 @@ const Toolbar = () => {
                     <Link to="/login">Login</Link>
                     <Link to="/create">Create Auction</Link>
                     <Link to="/bidhistory">Bid History</Link>
+                    <Link to="/loggout" onClick={loggout}>Logout</Link>
                 </div>
             </div>
             <div className="d-flex j-center">
+                {getUser !== null &&
                 <div className="profileInfo d-flex j-btw">
-                    <div>Logged as: {getUser.username}</div>
-                    <div className="d-flex f-column">
+                        <div>Logged as: {getUser.username}</div>
+                        <div className="d-flex f-column">
                         <div>Bank: {getUser.money} Eur</div>
-                    </div>
-                </div>
+                        </div>
+                </div>}
             </div>
         </div>
     );
